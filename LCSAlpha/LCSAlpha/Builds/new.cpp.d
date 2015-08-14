e7af30516359a84bca2a12c7d3b0695f3c505b19
Nick Burgel <nickburgel@me.com>
2015-08-14 11:40:48 -0400

    Stripping down content

---
+++ LCSAlpha/LCSAlpha/Builds/new.cpp.d
@@ -0,0 +1,3 @@
+Builds/new.cpp.o:  \
+ /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/new.cpp \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/new.h
